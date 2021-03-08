2eeea03e15d33e206e984539c24e29b849288c88
Nicolas Irving <nirving@darkedges.com>
2021-03-09 08:51:03 +1100

    Updates

--- virtualserver/versions.tf
+++ virtualserver/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     bigip = {
-      source = "f5networks/bigip"
-      version = ">= 1.7.0"
+      source = "darkedges.com/f5networks/bigip"
+      version = ">= 1.7.1"
     }
   }
   required_version = ">= 0.14"
